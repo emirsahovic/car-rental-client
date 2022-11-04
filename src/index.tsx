514b76fdff8849e68eaabf3c73a2763c8472d71b
Emir Šahović <emir.sahovic00@gmail.com>
2022-11-04 14:36:58 +0100

    Redux devtools disabled in production

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import { Provider } from 'react-redux';
-import reportWebVitals from './reportWebVitals';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { Provider } from "react-redux";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux/store";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,23 +1,23 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { authReducer } from './auth/reducers/authReducer';
-import { carReducer } from './car/reducers/carReducer';
-import { reservationReducer } from './reservation/reducers/reservationReducer';
-import { categoryReducer } from './category/reducers/categoryReducer';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { authReducer } from "./auth/reducers/authReducer";
+import { carReducer } from "./car/reducers/carReducer";
+import { reservationReducer } from "./reservation/reducers/reservationReducer";
+import { categoryReducer } from "./category/reducers/categoryReducer";
 
 const reducer = combineReducers({
-    auth: authReducer,
-    car: carReducer,
-    reservation: reservationReducer,
-    category: categoryReducer
+  auth: authReducer,
+  car: carReducer,
+  reservation: reservationReducer,
+  category: categoryReducer,
 });
 
-const initialState = {}
+// Uncomment when debugging
+// import { composeWithDevTools } from "redux-devtools-extension";
+// const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
 
-const middleware = [thunk];
-
-const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+// Comment out when debugging
+const store = createStore(reducer, applyMiddleware(thunk));
 
 export default store;
-export type RootState = ReturnType<typeof reducer>
+export type RootState = ReturnType<typeof reducer>;
